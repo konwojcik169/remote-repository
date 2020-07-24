b2478a4233587d4e99fb38fe73088b85ae87dbfe
Konrad Wojcik <kon.wojcik169gmail.co>
2020-07-24 14:57:02 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * main.c
+ *
+ *  Created on: 24 lip 2020
+ *      Author: kw131
+ */
+
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Hello, World!!!");
+   return 0;
+}
